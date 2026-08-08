1368c563a7bcc409ec06ada7b6d2480ba336c41a
{Bhanu <partapbhanu516@gmail.com>
2026-08-08 18:18:34 +0530

    Revising LangChain, covered all the theoritical convcetps it's main 6 componenets Models, Memory, Chains, Agents, Indexes, Prompts. Now, I have implemented LLM, will move forward towards, Agentic RAG Chatbot

--- Practice/ChatModels/chatModel1.py
+++ Practice/ChatModels/chatModel1.py
@@ -3,3 +3,8 @@ from dotenv   import load_dotenv
 
 load_dotenv()
 
+model = ChatOpenAI(model = 'gpt-4', temperature = 0.7, max_tokens = 1000)
+
+result = model.invoke("Write a short poem about the beauty of nature.")
+
+print(result)
