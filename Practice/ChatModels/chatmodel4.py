fff9873774f342c81229e388b5a7eac5de0c7c36
{Bhanu <partapbhanu516@gmail.com>
2026-08-10 17:46:14 +0530

    Yesterday. I learned how to use open source models in langchian

--- Practice/ChatModels/chatmodel4.py
+++ Practice/ChatModels/chatmodel4.py
@@ -1,16 +1,16 @@
-from langchain_huggingface import HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Hum directly Text-Generation API use karenge (jo free mein chalti hai)
+# Using a highly reliable supported model
 llm = HuggingFaceEndpoint(
     repo_id="mistralai/Mistral-7B-Instruct-v0.3", 
     task="text-generation",
     max_new_tokens=100
 )
 
-# ChatHuggingFace ko hata diya gaya hai.
-# Seedha llm.invoke() use karke prompt pass karenge:
-result = llm.invoke("Write a very short poem on Mahatma Gandhi.")
-print(result)
+model = ChatHuggingFace(llm=llm)
+result = model.invoke("Write a very short poem on Mahatma Gandhi.")
+print(result.content)
+# the code is not working
