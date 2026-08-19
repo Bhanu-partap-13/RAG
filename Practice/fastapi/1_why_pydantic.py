9c0e792ce35022b9b7382fe4b7709cccab9abf6c
{Bhanu <partapbhanu516@gmail.com>
2026-08-19 23:54:12 +0530

    learning pydantic in depth, and then i learn about the pydantic as for type validation in fast api

--- Practice/fastapi/1_why_pydantic.py
+++ Practice/fastapi/1_why_pydantic.py
@@ -1,19 +1,28 @@
 from pydantic import BaseModel
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
-    # ek pydantic model jiske andar data ayega from user
-    name : str
-    age : int
-    gender : str
 
-def insert_patient(patient: Patient):
-    print(patient.name) 
+    name: Annotated[str, Field(max_length=50, title='Name of the patient', description='Give the name of the patient in less than 50 chars', examples=['Nitish', 'Amit'])]
+    email: EmailStr
+    linkedin_url: AnyUrl
+    age: int = Field(gt=0, lt=120)
+    weight: Annotated[float, Field(gt=0, strict=True)]
+    married: Annotated[bool, Field(default=None, description='Is the patient married or not')]
+    allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
+    contact_details: Dict[str, str]
+
+
+def update_patient_data(patient: Patient):
+
+    print(patient.name)
     print(patient.age)
-    print(patient.gender)
-    print('Inserted')
+    print(patient.allergies)
+    print(patient.married)
+    print('updated')
 
-patient_info = {"name":'Awaiz' , "age":21, "gender":'Male'}
+patient_info = {'name':'nitish', 'email':'abc@gmail.com', 'linkedin_url':'http://linkedin.com/1322', 'age': '30', 'weight': 75.2,'contact_details':{'phone':'2353462'}}
 
 patient1 = Patient(**patient_info)
 
-insert_patient(patient1)
+update_patient_data(patient1)
